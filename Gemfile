ce747f9ac9259dd8b6054689594893180263c8dc
John Peter Harvey <john@johnpeterharvey.com>
2017-03-25 21:44:39 +0000

    Add start of pdf generation

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@
 source "https://rubygems.org"
 
 gem 'mini_magick'
-
+gem 'prawn'
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'mini_magick'
+require 'prawn'
 
 module MiniMagick
   class Image
@@ -22,60 +23,55 @@ cards_down   = 20
 card_width   = 70
 card_height  = 28
 
-output_mult   = 1 # Output image will be this * card_width wide, this * card_height tall
+output_mult   = 2 # Output image will be this * card_width wide, this * card_height tall
 output_width  = card_width * output_mult
 output_height = card_height * output_mult
 
-subimage_width = file.width / cards_across
-subimage_height = file.height / cards_down
+puts "Input image width #{file.width} height #{file.height}"
+puts "Mapped to output width #{output_width} height #{output_height}"
 
-p "Input image width #{file.width} height #{file.height}"
-p "Subimage width #{subimage_width} height #{subimage_height}"
-p "Mapped to output width #{output_width} height #{output_height}"
-
-# MiniMagick::Tool::Convert.new do |s|
-#   s.size "#{cards_across * output_width}x#{cards_down * output_height}"
-#   s.xc "white"
-#   s << "output/output.png"
-# end
-# output = MiniMagick::Image.new('output/output.png')
+print 'Writing large combined output...'
+file.resize("#{cards_across}x#{cards_down}!")
+    .sample("#{cards_across * output_width}x#{cards_down * output_height}!")
+    .write('output/output.png')
+puts 'done'
 
 for j in 0..cards_down - 1
   for i in 0..cards_across - 1
-    subimage_front = MiniMagick::Image.open('image.jpg').crop("#{subimage_width}x#{subimage_height}+#{i * subimage_width}+#{j * subimage_height}")
+    front_path = "output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.front.png"
+    card_front = MiniMagick::Image.open('output/output.png').crop("#{output_width}x#{output_height}+#{i * output_width}+#{j * output_height}")
+    card_front.write(front_path)
     # Get pixel color
-    subimage_front.scale('1x1')
-    color = subimage_front.pixel_at(0, 0)
-    # Scale up to output size
-    subimage_front.scale("#{[output_width, output_height].max}").crop("#{output_width}x#{output_height}+0+0")
-    
-    p "Writing front x#{i} y#{j}"
-    subimage_front.write("output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.front.png")
-    
+    card_front.scale('1x1')
+    color = card_front.pixel_at(0, 0)
+    puts "Completed front x#{i} y#{j} color #{color}"
+
     # Create the blank image for the background
-    # MiniMagick::Tool::Convert.new do |s|
-    #   s.size "#{output_width}x#{output_height}"
-    #   s.xc "black"
-    #   s << "output/#{i}.#{j}.back.png"
-    # end
+    back_path = "output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.back.png"
+    MiniMagick::Tool::Convert.new do |s|
+      s.size "#{output_width}x#{output_height}"
+      s.xc "black"
+      s << back_path
+    end
 
     # Add text to the back image and write the file out
-    # p "Writing back #{i} #{j}"
-    # MiniMagick::Image.new("output/#{i}.#{j}.back.png") do |s|
-    #   s.font 'Helvetica'
-    #   s.pointsize 40
-    #   s.gravity 'Center'
-    #   s.fill '#ffffff'
-    #   s.draw "text 0,0 #{color}"
-    #   s.write("output/#{i}.#{j}.back.png")
-    # end
-  
-    # Add to the large output image
-    # MiniMagick::Image.new('output/output.png').append("output/#{i}.#{j}.back.png").write('output/output.png')
+    MiniMagick::Image.new(back_path) do |s|
+      s.font 'Helvetica'
+      s.pointsize 40
+      s.gravity 'Center'
+      s.fill '#ffffff'
+      s.draw "text 0,0 #{color}"
+      s.write(back_path)
+    end
+    puts "Completed back #{i} #{j}"
 
-    #test_output = test_output.store_pixels(i * output_width, j * output_height, output_width, output_height, output_image)
+    Prawn::Document.generate("output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.pdf", :margin => 0, :page_size => [output_width, output_height]) do 
+      image front_path
+      start_new_page
+      image back_path
+    end
+    puts "Completed PDF #{i} #{j}"
   end
 end
 
-# test_output.write('output/output.png')
-
+puts 'Complete'
