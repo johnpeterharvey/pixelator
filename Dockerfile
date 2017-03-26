01593a1cb46bcc036d96e1680bae86dabf91f853
John Peter Harvey <john@johnpeterharvey.com>
2017-03-26 20:54:23 +0100

    More improvements

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,13 @@ RUN apt-get update \
  && apt-get dist-upgrade -y \
  && apt-get install -y graphicsmagick build-essential pkg-config ruby-dev
 
+VOLUME /input
 VOLUME /output
-ADD . /
+ADD Gemfile Gemfile.lock /
 
 RUN gem install bundler \
  && bundle install
 
-ENTRYPOINT ["/bin/bash"]
+ADD main.rb /
+
+ENTRYPOINT ["/usr/bin/ruby", "main.rb"]
--- main.rb
+++ main.rb
@@ -1,29 +1,18 @@
 require 'mini_magick'
 require 'prawn'
 
-module MiniMagick
-  class Image
-    def pixel_at x, y
-      run_command("convert", "#{path}[1x1+#{x.to_i}+#{y.to_i}]", 'txt:').split("\n").each do |line|
-        return $1 if /^0,0:.*(#[0-9a-fA-F]+)/.match(line)
-      end
-      nil
-    end
-  end
-end
-
 # MiniMagick.configure do |config|
 #   config.cli = :graphicsmagick
 # end
 
-file = MiniMagick::Image.open('image.jpg')
+file = MiniMagick::Image.open('input/image.jpg')
 
 cards_across = 20
 cards_down   = 20
 card_width   = 70
 card_height  = 28
 
-output_mult   = 2 # Output image will be this * card_width wide, this * card_height tall
+output_mult   = 20 # Output image will be this * card_width wide, this * card_height tall
 output_width  = card_width * output_mult
 output_height = card_height * output_mult
 
@@ -38,16 +27,16 @@ puts 'done'
 
 for j in 0..cards_down - 1
   for i in 0..cards_across - 1
-    front_path = "output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.front.png"
+    x_coord = i.to_s.rjust(2, "0")
+    y_coord = j.to_s.rjust(2, "0")
+
+    front_path = "output/x#{x_coord}y#{y_coord}.front.png"
     card_front = MiniMagick::Image.open('output/output.png').crop("#{output_width}x#{output_height}+#{i * output_width}+#{j * output_height}")
     card_front.write(front_path)
-    # Get pixel color
-    card_front.scale('1x1')
-    color = card_front.pixel_at(0, 0)
-    puts "Completed front x#{i} y#{j} color #{color}"
+    puts "Completed front x#{i} y#{j} "
 
     # Create the blank image for the background
-    back_path = "output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.back.png"
+    back_path = "output/x#{x_coord}y#{y_coord}.back.png"
     MiniMagick::Tool::Convert.new do |s|
       s.size "#{output_width}x#{output_height}"
       s.xc "black"
@@ -55,17 +44,18 @@ for j in 0..cards_down - 1
     end
 
     # Add text to the back image and write the file out
-    MiniMagick::Image.new(back_path) do |s|
+    card_back = MiniMagick::Image.new(back_path) do |s|
       s.font 'Helvetica'
-      s.pointsize 40
+      s.pointsize 96
       s.gravity 'Center'
       s.fill '#ffffff'
-      s.draw "text 0,0 #{color}"
-      s.write(back_path)
+      s.draw "text 0,0 'x #{x_coord} y #{y_coord}'"
     end
+    card_back.write(back_path)
+
     puts "Completed back #{i} #{j}"
 
-    Prawn::Document.generate("output/x#{i.to_s.rjust(2, "0")}y#{j.to_s.rjust(2, "0")}.pdf", :margin => 0, :page_size => [output_width, output_height]) do 
+    Prawn::Document.generate("output/x#{x_coord}y#{y_coord}.pdf", :margin => 0, :page_size => [output_width, output_height]) do 
       image front_path
       start_new_page
       image back_path
